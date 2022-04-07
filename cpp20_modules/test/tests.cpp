aca543e990a2b474a3136abb9c0a232e4f04c46e
Igar Yuzefovich <igar_yuzefovich@epam.com>
2022-04-07 11:57:49 +0400

    Add one more module

--- cpp20_modules/test/tests.cpp
+++ cpp20_modules/test/tests.cpp
@@ -2,10 +2,18 @@
 
 // clang-format off
 import math;
+import strings;
 import <cstdlib>;
+import <string_view>;
 // clang-format on
 
 // TEST(module_a, a) { ASSERT_EQ(2, ); }
 int main(int argc, char **argv) {
-  return 2 == math::sqrt(4) ? EXIT_SUCCESS : EXIT_FAILURE;
+
+  using namespace std::literals;
+  bool success = true;
+
+  success &= 2 == math::sqrt(4);
+  success &= "hello"sv == strings::hello();
+  return success ? EXIT_SUCCESS : EXIT_FAILURE;
 }
